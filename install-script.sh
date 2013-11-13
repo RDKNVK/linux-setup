30c07de8f05030a988ab32a418c9c7feb528846d
Radek Novak <radek.novak.cze@gmail.com>
2013-11-13 19:43:04 +0100

    added x-tile

--- install-script.sh
+++ install-script.sh
@@ -11,7 +11,7 @@ sudo apt-get install sqlite3 -y --force-yes
 # SUBLIME-TEXT
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt-get install sublime-text-installer -y 
 
 # g++ so I can compile latest node.js
 sudo apt-get install g++ -y --force-yes
@@ -29,3 +29,6 @@ export PATH=$PATH:/opt/node/bin
 
 # Or use the one liner to install the latest node.js
 #  bash < <(curl http://h3manth.com/njs)
+
+# some window tiling program
+sudo apt-get install x-tile -y --force-yes
