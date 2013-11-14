257d209b3a0691f59f4d520292e91a18b0f673a7
Radek Novák <radek.novak.cze@gmail.com>
2013-11-14 09:18:59 +0100

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -8,10 +8,16 @@ git config --global core.editor "vim"
 # SQLITE3
 sudo apt-get install sqlite3 -y --force-yes
 
-# SUBLIME-TEXT
+# add some repositories
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
+
+# SUBLIME-TEXT
 sudo apt-get install sublime-text-installer -y 
+# CHROME
+sudo apt-get install google-chrome-stable
 
 # g++ so I can compile latest node.js
 sudo apt-get install g++ -y --force-yes
@@ -26,6 +32,9 @@ cd node-v0.6.18
 ./configure && make && sudo make install
 
 export PATH=$PATH:/opt/node/bin
+
+
+
 
 # Or use the one liner to install the latest node.js
 #  bash < <(curl http://h3manth.com/njs)
